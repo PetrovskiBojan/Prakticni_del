60b84366337b82221eb50bad7746f664696d51a6
Bojan Petrovski <bojan.petrovski@novum-rgi.si>
2025-05-15 08:14:01 +0200

    feat: :sparkles: Naloga 3

--- src/data/validate_data.py
+++ src/data/validate_data.py
@@ -1,13 +1,32 @@
+import great_expectations as ge
 import pandas as pd
-from evidently.report import Report
-from evidently.metric_preset import DataDriftPreset
+import sys
+
+def validate(reference_path, current_path, report_path):
+    df = pd.read_csv(reference_path)
+    context = ge.get_context()
+    ref_df = ge.dataset.PandasDataset(df)
+    suite_name = "reference_suite"
+
+    try:
+        suite = context.get_expectation_suite(suite_name)
+    except Exception:
+        suite = context.create_expectation_suite(suite_name)
+
+    batch = ge.dataset.PandasDataset(ref_df)
+    batch.expect_column_to_exist("STEM_subjects")
+    batch.expect_column_to_exist("H_subjects")
+
+    context.save_expectation_suite(batch.get_expectation_suite(), suite_name)
+
+    current_df = ge.read_csv(current_path)
+    batch_to_validate = ge.dataset.PandasDataset(current_df, expectation_suite=suite)
+    results = batch_to_validate.validate()
+
+    with open(report_path, "w") as f:
+        f.write(results.to_json_dict().__str__())
+
+    print(results)
 
 if __name__ == "__main__":
-    report = Report(metrics=[DataDriftPreset()])
-
-    current = pd.read_csv("data/processed/current_data.csv")
-    reference = pd.read_csv("data/processed/reference_dataset.csv")
-
-    report.run(reference_data=reference, current_data=current)
-
-    report.save_html("reports/data_drift.html")
+    validate(sys.argv[1], sys.argv[2], sys.argv[3])
