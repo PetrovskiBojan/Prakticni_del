bcbc89216eddb514a6d32ac99cfc53ceb50e0065
Bojan Petrovski <bojan.petrovski@novum-rgi.si>
2025-05-15 07:52:31 +0200

    feat: :zap: Pipeline added

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 
-csv_df = pd.read_csv("../../data/raw/students_scores.csv")
-xlsx_df = pd.read_excel("../../data/raw/students.xlsx", engine="xlrd")
+csv_df = pd.read_csv("./data/raw/students_scores.csv")
+xlsx_df = pd.read_excel("./data/raw/students.xlsx")
 
 df = pd.concat([csv_df, xlsx_df], axis=1)
 
@@ -10,7 +10,7 @@ if "Unnamed: 0" in df.columns:
     df = df.drop(columns=["Unnamed: 0"])
 df["MIN_subject"] = df[["STEM_subjects", "H_subjects"]].min(axis=1)
 
-output_path = "../../data/processed/current_data.csv"
+output_path = "./data/processed/current_data.csv"
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
 df.to_csv(output_path, index=False)
